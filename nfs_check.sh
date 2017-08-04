81965eeeed6a16d3dafa60ebaca27df861faf91b
s1kr10s <s1kr10sdth@protonmail.com>
2017-08-04 13:20:07 -0400

    Update nfs_check.sh

--- nfs_check.sh
+++ nfs_check.sh
@@ -34,11 +34,33 @@ if [ `id -u` -eq 0 ]; then
 			list=(`showmount -e $ip | grep '*' |awk '{print $1}'`)
 			cont=$(showmount -e $ip | grep '*' |awk '{print $1}'|wc -l)
 			path=0
-			for i in $(seq 1 $cont)
-			do
-				echo "sudo mount -t nfs $ip:${list[$path]} /mnt -o nolock"
-				path=$((path+1))
-			done
+			change="_"
+			finds="/"
+			read -p "Do you want to mount the directory (Y/n)? " opc
+			echo
+			if [ "$opc" = "y" ]; then
+				echo "Loading Directory..."
+				for i in $(seq 1 $cont)
+				do
+					mk=$(echo ${list[$path]} | tr '[/]' '_')
+					mkdir /mnt/$mk
+					mount -t nfs $ip:${list[$path]} /mnt/$mk -o nolock
+					path=$((path+1))
+					if [ "$path" = "$cont" ]; then
+						echo "Load finished.."
+						echo "Opening Path /mnt..."
+						sleep 3
+						nautilus /mnt
+						echo
+					fi
+				done
+			else
+				for i in $(seq 1 $cont)
+				do
+					echo "sudo mount -t nfs $ip:${list[$path]} /mnt -o nolock"
+					path=$((path+1))
+				done
+			fi
 		else
 			echo
 			echo -e "\e[91mTHERE ISN'T TO SHOW :("
@@ -46,7 +68,7 @@ if [ `id -u` -eq 0 ]; then
 	else
 		echo
 		echo -e "\e[92mUsage: bash script.sh ip"
-		echo -e "\e[92mExample: bash script.sh 212.118.150.154"
+		echo -e "\e[92mExample: bash script.sh 212.118.127.132"
 		exit;
 	fi
 else
